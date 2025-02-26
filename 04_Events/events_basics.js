3ab507369e896221e57ca8399d465ed05f33a86f
VivekSingh0811 <1by23ai190@bmsit.in>
2025-02-27 01:01:20 +0530

    Events

---
+++ 04_Events/events_basics.js
@@ -0,0 +1,8 @@
+// In JS events run sequentially but sometimes we nreak the rule amd do not run it in sequence...we call it asynchronous programming...
+
+
+// IMPORTANT EVENTS TO STUDY .....
+// type, timestamp, defaultPrevented
+// target, toElement, srcElement, currentTarget,
+// clientX, clientY, screenX, screenY
+// altkey, ctrlkey, shiftkey, keyCode
